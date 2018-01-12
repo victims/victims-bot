bc5c2ab0cf9ef784bad4ab41d57f4f778a586a16
Steve Milner <smilner@redhat.com>
2018-01-12 08:57:49 -0500

    web: Update Hook with more logic
    Signed-off-by: Steve Milner <smilner@redhat.com>

--- web/handlers.go
+++ web/handlers.go
@@ -17,22 +17,40 @@ func Hook(w http.ResponseWriter, req *http.Request) {
 		log.Logger.Infof("could not read webhook: %s\n", err)
 		return
 	}
-
+	//body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Logger.Errorf("Web hook unable to read the request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	pushEvent := gh.PushEvent{}
+	//json.Unmarshal(body, &pushEvent)
 	hook.Extract(&pushEvent)
-	//TODO
-	// url := "git@github.com:victims/victims-cve-db.git"
-	// cloneDir, err := gh.Clone(url)
-	// if err != nil {
-	// 	log.Logger.Errorf("Web hook failed: %s", err)
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-	//
-	// for _, commit := range pushEvent.Commits {
-	// 	// Probably put this in it's own bounded goroutine
-	// 	for _, file := range commit.Added {
-	// 		gh.GetContent(cloneDir, commit.ID, file)
-	// 	}
-	// }
+	url := "https://github.com/victims/victims-cve-db.git" //"git@github.com:victims/victims-cve-db.git"
+	cloneDir, err := gh.Clone(url)
+	if err != nil {
+		log.Logger.Errorf("Web hook failed: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//fmt.Fprintf(w, cloneDir)
+
+	for _, commit := range pushEvent.Commits {
+		// Probably put this in it's own bounded goroutine
+		for _, file := range commit.Added {
+			contents, err := gh.GetContent(cloneDir, commit.ID, file)
+			if err != nil {
+				log.Logger.Errorf("Error getting contents: %s", err)
+			} else {
+				// TODO: Submit to the hash service
+				// TODO: Update the file
+				// newHash, err := gh.CommitChange(file)
+				// if err != nil {
+				// 	log.Logger.Errorf("Error committing change: %s", err)
+				// }
+				// TODO: Push results back to repo
+				log.Logger.Infof("%d", len(contents))
+			}
+		}
+	}
 }
